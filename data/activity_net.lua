6fb26d29f406dd00d7468649402baec4d8fdf61c
Sammer <sammer.puran1@students.unibe.ch>
2017-09-11 11:41:21 +0200

    only write watch part of youtube url to file

--- data/activity_net.lua
+++ data/activity_net.lua
@@ -2,10 +2,50 @@ require 'torch'
 
 local json = require 'json'
 
+function serialize(o)
+	
+	local output = io.open("videos.txt", "w")	
+	local text = ""	
+	for k,v in pairs(o) do  
+	pattern = "[?&]v=(...........)"
+	local id = string.match(v,pattern)	
+	print(id)	
+	
+	text=text.."\n"..id
+	end
+	
+	
+	output:write(text)
+	output:close()
+end
+
+
+	
+--print(data['jqKK2KH6l4Q'])
+	
 local file = io.open("activityNet.json")
-local table 
+local table1 
 if file then 
-	table = json.decode(file:read("*all"))
+	table1 = json.decode(file:read("*all"))
 	file:close() 
 end
-print(table)
+local urls = {}
+local annotations={}
+local labels = {}
+data = table1['database']
+for key,value in pairs(data)
+	do url = data[key]['url']
+	table.insert(urls,url)
+	annotations[url] = data[key]['annotations']
+	--print(annotations)
+	--for k,v in pairs(annotations)
+		--do label = annotations[k]['label']
+		--segment = annotations[k]['segment']
+			 
+		--print("key:"..key.." Segment: "..segment[1]..","..segment[2])
+	--end
+end
+--print(annotations)
+serialize(urls)
+
+
