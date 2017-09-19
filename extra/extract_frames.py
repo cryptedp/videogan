23b6c218a642088979d8f9f79cd7f0dd7ef2d91d
Sammer <sammer.puran1@students.unibe.ch>
2017-09-19 15:14:02 +0200

    python script now extracts frames automatically and creates new video for every action instance

--- extra/extract_frames.py
+++ extra/extract_frames.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 import os
-import ffmpeg
+import ffmpeg as ffmpeg
 import argparse
 import subprocess
 import random
@@ -14,27 +14,25 @@ def extract_frames(key,info):
     name = key+".mp4"
     abs_file_path = os.path.join(direct, name)
     if os.path.exists(abs_file_path):
-
         for i in frames:
             start= str(i[0])
             end= str(i[1])
+            trim_video(key,abs_file_path,start,end,frames.index(i))
 
 
-
-def trim_video(key,filename,startFrame,endFrame):
-    out_file = key+"out"+".mp4"
-    in_file = ffmpeg.input(filename)
-    in_file = ffmpeg.trim(in_file,start=startFrame,end=endFrame)
-    in_file = ffmpeg.output(os.path.join(direct, out_file))
-    ffmpeg._run(in_file)
-
-
-
+def trim_video(key,filename,startFrame,endFrame,index):
+    out_file = key+"out"+str(index)+".mp4"
+    file = os.path.join(direct, out_file)
+    (ffmpeg
+        .input(filename)
+        .trim(start=startFrame,end=endFrame)
+        .output(file)
+        .run())
 
 with open('../data/videos_segments.json') as data_file:
     data = json.load(data_file)
 
-for key,value in data.items():
+for key,value in data.items:
     extract_frames(key,data[key])
 
 
