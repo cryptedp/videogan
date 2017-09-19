42bf8ccf32cce494cee4333511381965a6bac288
Sammer <sammer.puran1@students.unibe.ch>
2017-09-19 14:29:38 +0200

    removed mistake, that one url cannot be associated with multiple frames

--- data/activity_net.lua
+++ data/activity_net.lua
@@ -18,7 +18,24 @@ function serialize(o)
 	output:write(text)
 	output:close()
 end
-
+function flatten_list(o,id)
+	
+	
+	segmentStarts = {}
+	segmentEnds={}
+	labels={}
+	for k,v in pairs(o)
+		do table.insert(segmentStarts,o[k]['segment'][1])
+		table.insert(segmentEnds,o[k]['segment'][2])
+		table.insert(labels,o[k]['label'])
+		
+		
+	end
+	dict= {["SegmentStart"]=segmentStarts,["SegmentEnd"]=segmentEnds,["label"]=label}
+	return dict 
+	
+	
+end
 
 	
 --print(data['jqKK2KH6l4Q'])
@@ -32,30 +49,43 @@ end
 local associations={}
 local urls = {}
 local annotations={}
+local annotation_real={}
 local labels = {}
 data = table1['database']
 for key,value in pairs(data)
 	do url = data[key]['url']
 	pattern = "[?&]v=(...........)"
 	local id = string.match(url,pattern)
+	
+		
 	table.insert(urls,id)
-	annotations[url] = data[key]['annotations']
+	annotations[id] = data[key]['annotations']
+	annotation_real[id]=flatten_list(annotations[id],id)
 	--print(annotations)
-	for k,v in pairs(annotations)
+	--print(annotations)
+	
+	--for k,v in pairs(annotations)
 		--do label = annotations[k]['label']
-		do annotation = annotations[k]
-		for k,v in pairs(annotation)
-			do segment = annotation[k]['segment']
-			label = annotation[k]['label']
-			associations[url]={["SegmentStart"]=segment[1],["SegmentEnd"]=segment[2],["label"]=label}
+		--do annotation = annotations[k]
+		
+
+		--for k,v in pairs(annotation)
+			--do print(k)
+			--do segment = annotation[k]['segment']
 			
-		end
+			--label = annotation[k]['label']
+			--associations[id]={["SegmentStart"]=segment[1],["SegmentEnd"]=segment[2],["label"]=label}
+			
+		--end
 		
 		--
-	end
+	--end
 end
 
-str = json.encode(associations)
+--print(annotation_real)
+
+--print(associations['6uhLrPgbpUA'])
+str = json.encode(annotation_real)
 local file= io.open("videos_segments.json", "w")
 file:write(str)
 file:close()	
--- extra/extract_frames.py
+++ extra/extract_frames.py
@@ -1,24 +1,41 @@
 import numpy as np
 import json
 import os
+import ffmpeg
 import argparse
 import subprocess
 import random
 from scipy.ndimage.filters import gaussian_filter
 
+direct = '/media/sammer/Seagate Backup Plus Drive/activitNet'
 
-from pprint import pprint
+def extract_frames(key,info):
+    frames = list(zip(info['SegmentStart'],info['SegmentEnd']))
+    name = key+".mp4"
+    abs_file_path = os.path.join(direct, name)
+    if os.path.exists(abs_file_path):
+
+        for i in frames:
+            start= str(i[0])
+            end= str(i[1])
+
+
+
+def trim_video(key,filename,startFrame,endFrame):
+    out_file = key+"out"+".mp4"
+    in_file = ffmpeg.input(filename)
+    in_file = ffmpeg.trim(in_file,start=startFrame,end=endFrame)
+    in_file = ffmpeg.output(os.path.join(direct, out_file))
+    ffmpeg._run(in_file)
+
+
+
 
 with open('../data/videos_segments.json') as data_file:
     data = json.load(data_file)
 
-def get_segments(datapoints):
-    segments = []
-    for key, value in datapoints.items():
-        segment_start = datapoints[key]['SegmentStart']
-        segment_end = datapoints[key]['SegmentEnd']
-        segments.append(tuple((segment_start,segment_end)))
+for key,value in data.items():
+    extract_frames(key,data[key])
 
-    return segments
 
-print(get_segments(data))
+
